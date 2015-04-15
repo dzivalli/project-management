7983e269a1546a828ef561e600cbc04bfdb1a5f1
Stas <stas@rostio.ru>
2015-04-15 12:19:13 +0300

    Fix find_invoice

--- app/controllers/robo_controller.rb
+++ app/controllers/robo_controller.rb
@@ -8,21 +8,21 @@ class RoboController < ApplicationController
   def result
     sum = params['OutSum']
     crc = params['SignatureValue']
-    if invoice && crc == invoice.pay_hash['crc'] && sum == invoice.pay_hash['out_summ']
-      invoice.get_paid!
-      client.update_plan!(invoice.plan) if invoice.plan
-      render plain: "OK#{invoice.id}"
+    if @invoice && crc == @invoice.pay_hash['crc'] && sum == @invoice.pay_hash['out_summ']
+      @invoice.get_paid!
+      client.update_plan!(invoice.plan) if @invoice.plan
+      render plain: "OK#{@invoice.id}"
     end
   end
 
   def success
-    if invoice
+    if @invoice
       redirect_to invoice_path(invoice), notice: 'Счет успешно оплачен'
     end
   end
 
   def failure
-    if invoice
+    if @invoice
       redirect_to invoice_path(invoice), alert: 'Счет не оплачен!'
     end
   end
@@ -30,6 +30,6 @@ class RoboController < ApplicationController
   private
 
   def find_invoice
-    invoice = Invoice.find_by(id: params['InvId'])
+    @invoice = Invoice.find_by(id: params['InvId'])
   end
 end
